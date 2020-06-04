7dfeec0aa66644f09cac217d77a391058a6c90f6
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-06-04 16:57:24 +0200

    Added responsive top navigation

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,20 @@
 console.log('It works!');
+
+/* ---------------------- Variables ---------------------- */
+const mobileMenuIcon = document.querySelector('.topnav button.icon');
+
+
+/* ---------------------- Function Definitions ---------------------- */
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function toggleMobileMenu() {
+    const navLinks = document.getElementById('navLinks');
+    if (navLinks.style.display === 'block') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'block';
+    }
+}
+
+/* ---------------------- Event Listeners ---------------------- */
+mobileMenuIcon.addEventListener('click', toggleMobileMenu);
